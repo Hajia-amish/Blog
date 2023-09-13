b215bf2c5cc2784f11d37b05b3852e4fe83edf91
hajia_ amish <hajiaamish533@gmail.com>
2023-09-13 18:18:54 +0000

    blog

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+import Details from './Component/Details';
+import Main from './Component/main';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Discover'>
+        <Stack.Screen
+          name="Discover"
+          component={Main}
+          options={{
+            headerTitle: () => (
+              <View style={styles.headerContainer}>
+                <Text style={styles.headerTitle}>Discover</Text>
+              </View>
+            ),
+            headerTitleAlign: 'center',
+          }}
+        />
+                <Stack.Screen
+          name="Beauty"
+          component={Details}
+          options={{
+            headerTitle: () => (
+              <View style={styles.headerContainer1}>
+                <Text style={styles.headerTitle1}>Beauty</Text>
+              </View>
+            ),
+            headerTitleAlign: 'left',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  headerContainer: {
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerTitle: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 24,
+    color:'#262742'
+  },
+  headerTitle1: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 24,
+    color:'#262742'
   },
 });
